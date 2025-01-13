48750f70e1f574237651a160253ab7befd524db8
Nuri <nurikaraca89@gmail.com>
2025-01-13 05:28:20 +0300

    Updated

--- app/api/files/upload/route.ts
+++ app/api/files/upload/route.ts
@@ -9,9 +9,9 @@ import { auth } from "@/auth";
 
 // Cloudinary Configuration
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
   
   
 });
--- app/api/user/image/route.ts
+++ app/api/user/image/route.ts
@@ -5,9 +5,9 @@ import { db } from "@/db";
 
 // Cloudinary Config
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
 });
 
 interface CloudinaryUploadResult {
