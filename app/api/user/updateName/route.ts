dd85ac71aa5e3ac55f09d5397efd8d7358fa14d3
Nuri <nurikaraca89@gmail.com>
2025-01-13 22:14:39 +0300

    updated

--- app/api/user/updateName/route.ts
+++ app/api/user/updateName/route.ts
@@ -2,42 +2,42 @@ import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/db";
 import { auth } from "@/auth";
 
-export const PUT = async (req: NextRequest) => {
-    const session = await auth();
-    if (!session || !session.user?.id) {
-        return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-    }
+// export const PUT = async (req: NextRequest) => {
+//     const session = await auth();
+//     if (!session || !session.user?.id) {
+//         return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+//     }
 
-    try {
-        const body = await req.json();
-        const { name } = body;
+//     try {
+//         const body = await req.json();
+//         const { name } = body;
 
-        if (!name || typeof name !== "string") {
-            return NextResponse.json(
-                { message: "Invalid input: 'name' must be a non-empty string." },
-                { status: 400 }
-            );
-        }
+//         if (!name || typeof name !== "string") {
+//             return NextResponse.json(
+//                 { message: "Invalid input: 'name' must be a non-empty string." },
+//                 { status: 400 }
+//             );
+//         }
         
-        const userId = session.user.id;
+//         const userId = session.user.id;
 
-        const updatedUser = await db.user.update({
-            where:{id: userId},
-            data: {name},
-        })
-        return NextResponse.json({
-            message: "User name updated successfully.",
-            user: updatedUser,
-          });
-        } catch (error) {
-          console.error("Error updating user name:", error);
+//         const updatedUser = await db.user.update({
+//             where:{id: userId},
+//             data: {name},
+//         })
+//         return NextResponse.json({
+//             message: "User name updated successfully.",
+//             user: updatedUser,
+//           });
+//         } catch (error) {
+//           console.error("Error updating user name:", error);
       
-          return NextResponse.json(
-            {
-              message: "An unexpected error occurred while updating user details.",
-              error: error instanceof Error ? error.message : "Unknown error",
-            },
-            { status: 500 }
-          );
-        }
-      };
+//           return NextResponse.json(
+//             {
+//               message: "An unexpected error occurred while updating user details.",
+//               error: error instanceof Error ? error.message : "Unknown error",
+//             },
+//             { status: 500 }
+//           );
+//         }
+//       };
