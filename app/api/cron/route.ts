16a11b7d5604afeb17e918d9fe641d1e17b8f27b
Nuri <nurikaraca89@gmail.com>
2025-01-13 21:18:46 +0300

    updated

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,22 +1,22 @@
-// import { NextResponse } from 'next/server';
-// import { db } from '@/db';
+import { NextResponse } from 'next/server';
+import { db } from '@/db';
 
-// export async function GET() {
-//   console.log('Cron job triggered to update expired tasks...');
-//   try {
-//     const now = new Date();
-//     const result = await db.task.updateMany({
-//       where: {
-//         dueDate: { lt: now },
-//         status: { not: 'CLOSED' },
-//       },
-//       data: { status: 'CLOSED' },
-//     });
+export async function GET() {
+  console.log('Cron job triggered to update expired tasks...');
+  try {
+    const now = new Date();
+    const result = await db.task.updateMany({
+      where: {
+        dueDate: { lt: now },
+        status: { not: 'CLOSED' },
+      },
+      data: { status: 'CLOSED' },
+    });
 
-//     console.log(`${result.count} tasks updated to CLOSED`);
-//     return NextResponse.json({ message: 'Cron job executed successfully', updatedCount: result.count });
-//   } catch (error) {
-//     console.error('Error updating tasks:', error);
-//     return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
-//   }
-// }
+    console.log(`${result.count} tasks updated to CLOSED`);
+    return NextResponse.json({ message: 'Cron job executed successfully', updatedCount: result.count });
+  } catch (error) {
+    console.error('Error updating tasks:', error);
+    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+  }
+}
